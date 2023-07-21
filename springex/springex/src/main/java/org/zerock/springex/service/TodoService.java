caec6031d1d943ac043f79b8db053d45333337f1
iian <dheyw@naver.com>
2023-07-21 11:25:55 +0900

    TodoServiceTests testRegister() 추가

---
+++ springex/springex/src/main/java/org/zerock/springex/service/TodoService.java
@@ -0,0 +1,9 @@
+package org.zerock.springex.service;
+
+import org.zerock.springex.dto.TodoDTO;
+
+public interface TodoService {
+
+    void register(TodoDTO todoDTO);
+
+}
--- springex/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
+++ springex/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
@@ -1,4 +1,4 @@
-package org.zerock.springex.sample.mapper;
+package org.zerock.springex.mapper;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
