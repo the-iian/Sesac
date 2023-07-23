d45a71dd1ec3e3c1ba53e0ee11471bad255357d5
iian <dheyw@naver.com>
2023-07-23 19:37:29 +0900

    TodoMapper count 처리

--- springex/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
+++ springex/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.zerock.springex.domain.TodoVO;
+import org.zerock.springex.dto.PageRequestDTO;
 import org.zerock.springex.mapper.TodoMapper;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Log4j2
 @ExtendWith(SpringExtension.class)
@@ -42,5 +44,18 @@ public class TodoMapperTests {
         TodoVO todoVO = todoMapper.selectOne(3L);
 
         log.info(todoVO);
+    }
+
+    @Test
+    public void testSelectList(){
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        List<TodoVO> voList = todoMapper.selectList(pageRequestDTO);
+
+        voList.forEach(vo -> log.info(vo));
     }
 }
