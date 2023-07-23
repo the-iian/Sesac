e4150eab77f83862a7d9d0373f8d65b876710a6d
iian <dheyw@naver.com>
2023-07-24 00:04:15 +0900

    spring web mvc final

--- springex/springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -102,6 +102,5 @@ public class TodoController {
             pageRequestDTO = PageRequestDTO.builder().build();
         }
         model.addAttribute("responseDTO", todoService.getList(pageRequestDTO));
-
     }
 }
