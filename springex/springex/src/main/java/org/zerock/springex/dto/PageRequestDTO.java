215b2b6d023a734f97c184c513598cf8775dc68c
iian <dheyw@naver.com>
2023-07-23 20:55:42 +0900

    조회를 위한 링크 처리

--- springex/springex/src/main/java/org/zerock/springex/dto/PageRequestDTO.java
+++ springex/springex/src/main/java/org/zerock/springex/dto/PageRequestDTO.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Builder
 @Data
@@ -28,6 +30,12 @@ public class PageRequestDTO {
 
     private String link;
 
+    private String[] types;
+    private String keyword;
+    private boolean finished;
+    private LocalDate from;
+    private LocalDate to;
+
     public int getSkip(){
 
         return (page -1) * 10;
@@ -42,5 +50,14 @@ public class PageRequestDTO {
         }
 
         return link;
+    }
+
+    public boolean checkType(String type){
+
+        if (types == null || types.length == 0){
+            return false;
+        }
+
+        return Arrays.stream(types).anyMatch(type::equals);
     }
 }
