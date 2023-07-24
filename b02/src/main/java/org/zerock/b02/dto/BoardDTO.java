09e5b9c8723053db861ebe25bb2d490218c523d1
iian <dheyw@naver.com>
2023-07-24 17:04:28 +0900

    등록처리와 화면 개발

--- b02/src/main/java/org/zerock/b02/dto/BoardDTO.java
+++ b02/src/main/java/org/zerock/b02/dto/BoardDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -13,9 +16,17 @@ import java.time.LocalDateTime;
 public class BoardDTO {
 
     private Long bno;
+
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
+
+    @NotEmpty
     private String content;
+
+    @NotEmpty
     private String writer;
+
     private LocalDateTime regDate;
     private LocalDateTime modDate;
 }
