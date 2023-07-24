ab24b8532b4ddb17c2e419c68ebbd820a8b2c6ba
iian <dheyw@naver.com>
2023-07-24 16:40:54 +0900

    목록/검색기능 선언

--- b02/src/main/java/org/zerock/b02/service/BoardService.java
+++ b02/src/main/java/org/zerock/b02/service/BoardService.java
@@ -1,6 +1,9 @@
 package org.zerock.b02.service;
 
+import org.zerock.b02.domain.Board;
 import org.zerock.b02.dto.BoardDTO;
+import org.zerock.b02.dto.PageRequestDTO;
+import org.zerock.b02.dto.PageResponseDTO;
 
 public interface BoardService {
 
@@ -11,4 +14,6 @@ public interface BoardService {
     void modify(BoardDTO boardDTO);
 
     void remove(Long bno);
+
+    PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
 }
--- b02/src/main/java/org/zerock/b02/service/BoardServiceImpl.java
+++ b02/src/main/java/org/zerock/b02/service/BoardServiceImpl.java
@@ -3,13 +3,20 @@ package org.zerock.b02.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.zerock.b02.domain.Board;
 import org.zerock.b02.dto.BoardDTO;
+import org.zerock.b02.dto.PageRequestDTO;
+import org.zerock.b02.dto.PageResponseDTO;
 import org.zerock.b02.repository.BoardRepository;
 
 import javax.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
@@ -59,5 +66,25 @@ public class BoardServiceImpl implements BoardService{
     public void remove(Long bno){
 
         boardRepository.deleteById(bno);
+    }
+
+    @Override
+    public PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO){
+
+        String[] types = pageRequestDTO.getTypes();
+        String keyword = pageRequestDTO.getKeyword();
+
+        Pageable pageable = pageRequestDTO.getPageable("bno");
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
+
+        List<BoardDTO> dtoList = result.getContent().stream()
+                .map(board -> modelMapper.map(board, BoardDTO.class)).collect(Collectors.toList());
+
+        return PageResponseDTO.<BoardDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
+
     }
 }
