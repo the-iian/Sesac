d1f6f917bdd4e4a42e14ce995e9f3fd25e4294ef
iian <dheyw@naver.com>
2023-07-24 17:21:11 +0900

    수정 처리

--- b02/src/main/java/org/zerock/b02/controller/BoardController.java
+++ b02/src/main/java/org/zerock/b02/controller/BoardController.java
@@ -71,4 +71,29 @@ public class BoardController {
 
         model.addAttribute("dto", boardDTO);
     }
+
+    @PostMapping("/modify")
+    public String modify(PageRequestDTO pageRequestDTO,
+                         @Valid BoardDTO boardDTO,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes){
+
+        log.info("board modify post....." + boardDTO);
+
+        if (bindingResult.hasErrors()){
+            log.info("has errors....");
+
+            String link = pageRequestDTO.getLink();
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+            return "redirect:/board/modify?"+link;
+        }
+
+        boardService.modify(boardDTO);
+        redirectAttributes.addFlashAttribute("result", "modified");
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+        return "redirect:/board/read";
+    }
 }
