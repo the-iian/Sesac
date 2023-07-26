c1fb5c37dc9edf86c4192df53a3143c0ba8a8d59
iian <dheyw@naver.com>
2023-07-26 15:32:35 +0900

    REST 방식의 서비스

---
+++ b02/src/main/java/org/zerock/b02/controller/ReplyController.java
@@ -0,0 +1,37 @@
+package org.zerock.b02.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b02.dto.ReplyDTO;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/replies")
+@Log4j2
+public class ReplyController {
+
+    @ApiOperation(value = "Replies POST", notes = "POST방식으로 댓글 등록")
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String,Long> register(
+            @Valid @RequestBody ReplyDTO replyDTO,
+            BindingResult bindingResult) throws BindException {
+
+        log.info(replyDTO);
+
+        if(bindingResult.hasErrors()){
+            throw new BindException(bindingResult);
+        }
+
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", 111L);
+
+        return resultMap;
+    }
+}
