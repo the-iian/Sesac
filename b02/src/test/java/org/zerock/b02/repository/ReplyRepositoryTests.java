0ba80b79b02d9b4068287a18a09b13d18213060b
iian <dheyw@naver.com>
2023-07-27 20:03:33 +0900

    testBoardReplies() 디버깅 필요

--- b02/src/test/java/org/zerock/b02/repository/ReplyRepositoryTests.java
+++ b02/src/test/java/org/zerock/b02/repository/ReplyRepositoryTests.java
@@ -4,8 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.zerock.b02.domain.Board;
 import org.zerock.b02.domain.Reply;
+
+import java.awt.print.Pageable;
 
 @SpringBootTest
 @Log4j2
@@ -30,4 +35,18 @@ public class ReplyRepositoryTests {
 
         replyRepository.save(reply);
     }
+
+//    @Test
+//    public void testBoardReplies() {
+//
+//        Long bno = 100L;
+//
+//        Pageable pageable = PageRequest.of(0, 10, Sort.by("rno").descending());
+//
+//        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+//
+//        result.getContent().forEach(reply -> {
+//            log.info(reply);
+//        });
+//    }
 }
