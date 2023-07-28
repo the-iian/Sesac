57f2e1a4ca20ab116aad92ec246d6718f1ba4733
iian <dheyw@naver.com>
2023-07-28 13:16:51 +0900

    BoardSearch 메서드 추가

--- b02/src/main/java/org/zerock/b02/repository/search/BoardSearch.java
+++ b02/src/main/java/org/zerock/b02/repository/search/BoardSearch.java
@@ -3,10 +3,13 @@ package org.zerock.b02.repository.search;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.zerock.b02.domain.Board;
+import org.zerock.b02.dto.BoardListReplyCountDTO;
 
 public interface BoardSearch {
 
     Page<Board> search1(Pageable pageable);
 
     Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+
+    Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable);
 }
