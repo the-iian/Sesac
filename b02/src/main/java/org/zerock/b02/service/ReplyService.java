668903b02694780a0fd449e957a02402cc840926
iian <dheyw@naver.com>
2023-07-28 15:14:07 +0900

    특정 게시물의 댓글 목록 처리

--- b02/src/main/java/org/zerock/b02/service/ReplyService.java
+++ b02/src/main/java/org/zerock/b02/service/ReplyService.java
@@ -1,5 +1,7 @@
 package org.zerock.b02.service;
 
+import org.zerock.b02.dto.PageRequestDTO;
+import org.zerock.b02.dto.PageResponseDTO;
 import org.zerock.b02.dto.ReplyDTO;
 
 public interface ReplyService {
@@ -11,4 +13,6 @@ public interface ReplyService {
     void modify(ReplyDTO replyDTO);
 
     void remove(Long rno);
+
+    PageResponseDTO<ReplyDTO> getListOfBoard(Long bno, PageRequestDTO pageRequestDTO);
 }
