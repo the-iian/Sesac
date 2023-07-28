a9d26152a874ae030c2c434fc33e3246c8f161b5
iian <dheyw@naver.com>
2023-07-28 16:31:34 +0900

    특정 댓글 수정

--- b02/src/main/java/org/zerock/b02/controller/ReplyController.java
+++ b02/src/main/java/org/zerock/b02/controller/ReplyController.java
@@ -76,4 +76,19 @@ public class ReplyController {
 
         return resultMap;
     }
+
+    @ApiOperation(value = "Modify Reply", notes = "PUT 방식으로 특정 댓글 수정")
+    @PutMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String,Long> remove(@PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO){
+
+        replyDTO.setRno(rno); // 번호를 일치시킴
+
+        replyService.modify(replyDTO);
+
+        Map<String, Long> resultMap = new HashMap<>();
+
+        resultMap.put("rno", rno);
+
+        return resultMap;
+    }
 }
