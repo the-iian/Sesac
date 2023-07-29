b8a3046df3001b6c6d957dc229a87a2eb02b886f
iian <dheyw@naver.com>
2023-07-30 02:45:25 +0900

    update

--- b02/src/main/java/org/zerock/b02/dto/ReplyDTO.java
+++ b02/src/main/java/org/zerock/b02/dto/ReplyDTO.java
@@ -6,11 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-
 
 
 @Data
