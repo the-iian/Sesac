5cbe7e2caa09eb1b9dad2b9a79f728ea52e87415
iian <dheyw@naver.com>
2023-07-31 18:58:04 +0900

    파일 저장경로를 위한 @Value 처리

--- b02/src/main/java/org/zerock/b02/controller/UpDownController.java
+++ b02/src/main/java/org/zerock/b02/controller/UpDownController.java
@@ -1,19 +1,19 @@
 package org.zerock.b02.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b02.dto.upload.UploadFileDTO;
 
-import javax.validation.Valid;
 
 @RestController
 @Log4j2
 public class UpDownController {
 
-    @Valid("${org.zerock.upload.path}") // import시 springframework로 시작하는 value
+    @Value ("${org.zerock.upload.path}") // import시 springframework로 시작하는 value
     private String uploadPath;
 
     @ApiOperation(value = "Upload POST", notes = "POST방식으로 파일 등록")
