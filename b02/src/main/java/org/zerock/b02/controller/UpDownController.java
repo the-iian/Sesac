3ccd1fdc677ece1bc9751cb7e870a9a8beed8787
iian <dheyw@naver.com>
2023-07-31 19:21:39 +0900

    UpDownController upload() - List<UploadResultDTO> 반환처리

--- b02/src/main/java/org/zerock/b02/controller/UpDownController.java
+++ b02/src/main/java/org/zerock/b02/controller/UpDownController.java
@@ -8,12 +8,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b02.dto.upload.UploadFileDTO;
+import org.zerock.b02.dto.upload.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -21,20 +24,22 @@ import java.util.UUID;
 @Log4j2
 public class UpDownController {
 
-    @Value ("${org.zerock.upload.path}") // import시 springframework로 시작하는 value
+    @Value("${org.zerock.upload.path}") // import시 springframework로 시작하는 value
     private String uploadPath;
 
     @ApiOperation(value = "Upload POST", notes = "POST방식으로 파일 등록")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
-    public String upload(UploadFileDTO uploadFileDTO){
+    public String upload(UploadFileDTO uploadFileDTO) {
 
         log.info(uploadFileDTO);
 
-        if (uploadFileDTO.getFiles() != null){
+        if (uploadFileDTO.getFiles() != null) {
+
+            final List<UploadResultDTO> list = new ArrayList<>();
 
             uploadFileDTO.getFiles().forEach(multipartFile -> {
-                
+
                 // 중복된 파일명 방지
                 String originalName = multipartFile.getOriginalFilename();
                 log.info(originalName);
@@ -42,23 +47,35 @@ public class UpDownController {
                 // UUID로 새로운 코드값 생성
                 String uuid = UUID.randomUUID().toString();
 
-                Path savePath = Paths.get(uploadPath, uuid+"_"+ originalName);
+                Path savePath = Paths.get(uploadPath, uuid + "_" + originalName);
 
-                try{
+                boolean image = false;
+
+                try {
                     multipartFile.transferTo(savePath); // 실제 파일 저장
 
                     // 이미지 파일이라면
                     if (Files.probeContentType(savePath).startsWith("image")) {
 
+                        image = true;
+
                         File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
 
-                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200,200);
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
                     }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
+                list.add(UploadResultDTO.builder()
+                        .uuid(uuid)
+                        .fileName(originalName)
+                        .img(image).build());
+
             }); // end each
+
+            return list;
         } // end if
 
         return null;
