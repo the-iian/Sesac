9c3d9557d542cbb4fdadbfe2e60c62740ce69338
iian <dheyw@naver.com>
2023-07-31 21:49:57 +0900

    댓글 갯수와 DTO처리

---
+++ b02/src/main/java/org/zerock/b02/dto/BoardImageDTO.java
@@ -0,0 +1,17 @@
+package org.zerock.b02.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardImageDTO {
+
+    private String uuid;
+    private String fileName;
+    private int ord;
+}
