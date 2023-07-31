d278fa6f2923177f02d303277fff4d4a76382f7f
iian <dheyw@naver.com>
2023-07-31 22:21:37 +0900

    DTO를 엔티티로 변환

--- b02/src/main/java/org/zerock/b02/service/BoardService.java
+++ b02/src/main/java/org/zerock/b02/service/BoardService.java
@@ -20,4 +20,23 @@ public interface BoardService {
 
     // 게시글의 이미지와 댓글의 숫자까지 처리
     PageResponseDTO<BoardListAllDTO> listWithAll(PageRequestDTO pageRequestDTO);
+
+    default Board dtoToEntity(BoardDTO boardDTO){
+
+        Board board = Board.builder()
+                .bno(boardDTO.getBno())
+                .title(boardDTO.getTitle())
+                .content(boardDTO.getContent())
+                .writer(boardDTO.getWriter())
+                .build();
+
+        if (boardDTO.getFileNames() != null){
+            boardDTO.getFileNames().forEach(fileName -> {
+
+                String[] arr = fileName.split("_");
+                board.addImage(arr[0], arr[1]);
+            });
+        }
+        return board;
+    }
 }
