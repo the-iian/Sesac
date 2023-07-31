2890a7f6bc88566cd68643a1d56ed05f9cc89d13
iian <dheyw@naver.com>
2023-07-31 18:48:58 +0900

    컨트롤러와 Swagger UI 테스트

--- b02/src/main/java/org/zerock/b02/controller/UpDownController.java
+++ b02/src/main/java/org/zerock/b02/controller/UpDownController.java
@@ -1,4 +1,23 @@
 package org.zerock.b02.controller;
 
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.b02.dto.upload.UploadFileDTO;
+
+@RestController
+@Log4j2
 public class UpDownController {
+
+    @ApiOperation(value = "Upload POST", notes = "POST방식으로 파일 등록")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+
+    public String upload(UploadFileDTO uploadFileDTO){
+
+        log.info(uploadFileDTO);
+
+        return null;
+    }
 }
