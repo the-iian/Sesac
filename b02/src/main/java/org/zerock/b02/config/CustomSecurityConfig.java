26c5f61724d19c4ac04128cce5c89e7436d136d3
iian <dheyw@naver.com>
2023-08-01 18:49:53 +0900

    스프링 시큐리티 설정

---
+++ b02/src/main/java/org/zerock/b02/config/CustomSecurityConfig.java
@@ -0,0 +1,11 @@
+package org.zerock.b02.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+
+@Log4j2
+@Configuration
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+}
