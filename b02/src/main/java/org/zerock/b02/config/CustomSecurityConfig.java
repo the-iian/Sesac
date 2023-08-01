ea45fd56ca220a7ec3c0dceb29831c0c1cda4a59
iian <dheyw@naver.com>
2023-08-01 19:27:51 +0900

    로그인 페이지 구성

--- b02/src/main/java/org/zerock/b02/config/CustomSecurityConfig.java
+++ b02/src/main/java/org/zerock/b02/config/CustomSecurityConfig.java
@@ -5,22 +5,29 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomSecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         log.info("--------------configure--------------");
 
-        http.formLogin();
+        http.formLogin().loginPage("/member/login");
 
         return http.build();
     }
@@ -31,6 +38,5 @@ public class CustomSecurityConfig {
         log.info("--------------web configure--------------");
 
         return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-
     }
 }
--- b02/src/main/java/org/zerock/b02/controller/BoardController.java
+++ b02/src/main/java/org/zerock/b02/controller/BoardController.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -48,7 +49,7 @@ public class BoardController {
         model.addAttribute("responseDTO", responseDTO);
     }
 
-
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/register")
     public void registerGET(){
 
