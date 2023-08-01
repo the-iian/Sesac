0d6cc8c11adf94771f97a96a9ca6b86504292239
iian <dheyw@naver.com>
2023-08-02 01:45:00 +0900

    706p 데이터베이스에 쿠키 유지안됨

--- b02/src/main/java/org/zerock/b02/controller/BoardController.java
+++ b02/src/main/java/org/zerock/b02/controller/BoardController.java
@@ -11,19 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b02.dto.*;
 import org.zerock.b02.service.BoardService;
-
 import javax.validation.Valid;
 import java.io.File;
 import java.nio.file.Files;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/board")
