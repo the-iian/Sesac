c9876c728e07ccae9a224e9a27b8c4ae4e1c92ec
iian <dheyw@naver.com>
2023-08-20 19:43:55 +0900

    Mock Api 호출 Testcode

---
+++ apiBaas/src/test/java/com/example/apibaas/RestTemplateTestServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.apibaas;
+
+import com.example.apibaas.controller.RestTemplateTestController;
+import com.example.apibaas.model.PasswordResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+public class RestTemplateTestServiceTest {
+
+    @Autowired
+    RestTemplateTestController controller;
+
+    @Test
+    public void restTemplateTest(){
+        ResponseEntity<PasswordResponse> entity = controller.restTemplateTest1();
+        System.out.println(">>> entity = " + entity.getStatusCode());
+        Assertions.assertEquals(entity.getStatusCodeValue(), 200);
+    }
+}
