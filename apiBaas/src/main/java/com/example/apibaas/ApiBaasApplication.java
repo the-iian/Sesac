b2295ad469cdacd6fa9b248e616fea943ce7f33c
iian <dheyw@naver.com>
2023-08-20 18:41:47 +0900

    setting

---
+++ apiBaas/src/main/java/com/example/apibaas/ApiBaasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apibaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBaasApplication.class, args);
+    }
+
+}
