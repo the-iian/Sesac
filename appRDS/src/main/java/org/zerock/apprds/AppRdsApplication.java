8c337dbf8425576d45518edb64a4dd83f5bcf9ed
iian <dheyw@naver.com>
2023-09-17 15:25:45 +0900

    ㅤ

--- appRDS/src/main/java/org/zerock/apprds/AppRdsApplication.java
+++ appRDS/src/main/java/org/zerock/apprds/AppRdsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppRdsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AppRdsApplication.class, args);
     }
